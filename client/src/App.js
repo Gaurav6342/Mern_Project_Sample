ec82724e314245769ae834604ba0f8669d6095ec
Ggsb@773294 <gaurav.mfworld1997@gmail.com>
2023-12-11 13:37:27 +0530

    backend is conect to fronend

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   // Fetching message from backend on mount
   useEffect(() => {
-    fetch("http://localhost:4000")
+    fetch("https://mern-project-sample.onrender.com")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
